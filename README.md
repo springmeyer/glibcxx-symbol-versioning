99bdc92e38a253f46fa6c2d0e178f3827b8d1c12
Dane Springmeyer <dane@mapbox.com>
2016-09-30 22:45:23 -0700

    Add travis badge/code comment

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # glibcxx-symbol-versioning
+
 Sample code to demonstrate how libstdc++ symbol versioning works
+
+[![Build Status](https://travis-ci.org/springmeyer/glibcxx-symbol-versioning.svg?branch=master)](https://travis-ci.org/springmeyer/glibcxx-symbol-versioning)
--- test_runtime_error_cstring.cpp
+++ test_runtime_error_cstring.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     try {
+        // https://github.com/gcc-mirror/gcc/blob/d7b41a73a78cbd6f291bf8d4090638057b964c11/libstdc%2B%2B-v3/config/abi/pre/gnu.ver#L1618-L1835
         throw std::runtime_error("hello");
     } catch (std::exception const& ex) {
         std::clog << ex.what() << "\n";
