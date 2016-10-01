1fb4a10e2d8998ce131f5584130676629a805f0e
Dane Springmeyer <dane@mapbox.com>
2016-09-30 22:06:29 -0700

    use nm to display symbol name as well as GLIBCXX version

--- test.sh
+++ test.sh
@@ -14,9 +14,19 @@ export PATH=$(./.mason/mason prefix ${PACKAGE_NAME} ${CLANG_VERSION})/bin:${PATH
 
 function color_echo    { >&2 echo -e "\033[1m\033[36m* $1\033[0m"; }
 
+function check() {
+    RESULT=0
+    nm test_runtime_error | grep GLIBCXX_3.4.2 > /tmp/out.txt || RESULT=$?
+    if [[ ${RESULT} != 0 ]]; then
+        echo "GLIBCXX_3.4.2 not found"
+    else
+        cat /tmp/out.txt | c++filt
+    fi
+}
+
 color_echo "runtime c++98 (${HEADERS})"
 clang++ test_runtime_error.cpp -o test_runtime_error -std=c++98
-strings test_runtime_error | grep GLIBCXX_3.4.2 || echo "GLIBCXX_3.4.2 not found"
+check
 
 color_echo "runtime c++11 (${HEADERS})"
 clang++ test_runtime_error.cpp -o test_runtime_error -std=c++11
