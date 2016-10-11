d6c2e417c19b6ef56be67c2f6038ec846f7bf0dc
Dane Springmeyer <dane@mapbox.com>
2016-10-10 23:56:47 -0400

    fix compile on osx

--- test_get_new_handler.cpp
+++ test_get_new_handler.cpp
@@ -3,24 +3,29 @@
 #include <new>
 
 // http://stackoverflow.com/questions/31506594/how-do-i-test-the-version-of-libstdc-not-gcc-at-compile-time
-#ifndef __GLIBCXX__
-    #error __GLIBCXX__ must be defined
-#elif __GLIBCXX__ == 20120301 // 4.8 on vanilla precise
-    // does not work
-#elif __GLIBCXX__ == 20130604 // 4.8 upgraded on precise
-    // does not work
-#elif __GLIBCXX__ == 20150426 // 4.8 on vanilla precise
-    // does not work
+#ifdef __linux__
+    #ifndef __GLIBCXX__
+        #error __GLIBCXX__ must be defined
+    #elif __GLIBCXX__ == 20120301 // 4.8 on vanilla precise
+        // does not work
+    #elif __GLIBCXX__ == 20130604 // 4.8 upgraded on precise
+        // does not work
+    #elif __GLIBCXX__ == 20150426 // 4.8 on vanilla precise
+        // does not work
+    #else
+        // we assume it works: known version that work from ubuntu-toolchain-r are:
+        // 4.9: 20160726
+        // 5:   20160904
+        // 6:   20160901
+        #define SUPPORTS_NEW_HANDLER
+    #endif
 #else
-    // we assume it works: known version that work from ubuntu-toolchain-r are:
-    // 4.9: 20160726
-    // 5:   20160904
-    // 6:   20160901
-    #define GLIBCXX_SUPPORTS_NEW_HANDLER
+    #define SUPPORTS_NEW_HANDLER
 #endif
 
 int main() {
-#if __cplusplus >= 201300L && defined(GLIBCXX_SUPPORTS_NEW_HANDLER)
+#if __cplusplus >= 201300L && defined(SUPPORTS_NEW_HANDLER)
+    // https://github.com/gcc-mirror/gcc/blob/d7b41a73a78cbd6f291bf8d4090638057b964c11/libstdc%2B%2B-v3/config/abi/pre/gnu.ver#L1599-L1602
     std::new_handler nh = std::get_new_handler();
     if (nh) {
         std::clog << "new handler works\n";
