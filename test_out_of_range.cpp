2e151b987adf6e44035da46fa728737c5afe3307
Dane Springmeyer <dane@mapbox.com>
2017-04-19 20:02:42 -0700

    use array to try to prompt throw_out_of_range usage

--- test_out_of_range.cpp
+++ test_out_of_range.cpp
@@ -1,8 +1,15 @@
 #include "cxx14_workaround.hpp"
 #include <iostream>
 #include <stdexcept>
+#include <array>
 
+#ifdef __linux__ 
 #define WORKAROUND
+ #warning building with workaround
+#else
+ #warning not building with workaround
+#endif
+
 // https://github.com/bitcoin/bitcoin/pull/4042
 // allows building against libstdc++-dev-4.9 while avoiding
 // GLIBCXX_3.4.20 dep
@@ -28,6 +35,8 @@ int main() {
         // https://github.com/gcc-mirror/gcc/blob/d7b41a73a78cbd6f291bf8d4090638057b964c11/libstdc%2B%2B-v3/include/std/array#L208
         // https://gcc.gnu.org/ml/libstdc++/2013-09/txtxoep3wCwLb.txt
         // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60936
+        std::array<int, 3> a1{ {1, 2, 3} };
+        auto item = a1.at(0);
         throw std::out_of_range("hello");
     } catch (std::exception const& ex) {
         std::clog << ex.what() << "\n";
